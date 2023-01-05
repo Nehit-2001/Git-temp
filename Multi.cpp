b6e0c9a8980d4f6bb13509c3913f5d4230ca945b
Mohit <mohitsn444@gmail.com>
2023-01-05 23:32:23 +0530

    Added more codes

---
+++ Multi.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a=5, b=6;
+    int c;
+
+    c = a*b;
+    cout<<"Multiply = "<<c<<endl;
+    return 0;
+}
---
+++ Sub.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a=10, b=3;
+    int c;
+
+    c = abs(b-a);
+    int d = a%2;
+    cout<<d<<endl;
+    cout<<"Sub = "<<c<<endl;
+    return 0;
+}
